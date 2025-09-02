17f9e20aa63661060eb06545f95905c022b1c23d
jarrendevs <johnarren15novilla@gmail.com>
2025-09-02 20:49:21 +0800

    Install nextAuth.js package

--- src/app/api/auth/google/route.ts
+++ src/app/api/auth/google/route.ts
@@ -2,15 +2,5 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const response = await fetch("http://localhost:3000/auth/google");
-  const data = await response.json();
-  return NextResponse.json(data);
+  return NextResponse.json({message: "google oAuth redirect"});
 }
-
-// export async function POST(request: NextRequest) {
-//   console.log(request.body, "check");
-//   const formData = await request.formData();
-//   const formDataObj = Object.fromEntries(formData);
-//   console.log(formDataObj);
-//   return NextResponse.json({ provider: "Google Auth Post" });
-// }
